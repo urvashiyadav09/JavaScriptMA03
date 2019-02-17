f06f1604dfac9666370e13ad446b808946f7d613
Urvashi Devi <urvashiyadav09@gmail.com>
2019-02-17 23:36:57 +0100

    query 1 and level 2 report added

---
+++ query1/jsfile.js
@@ -0,0 +1,27 @@
+document.getElementById("submit").addEventListener("click", function() {
+
+  //  	var fnameInput = document.getelementsByName("f_name");
+  //    var lnameInput = document.getelementsByName("l_name");
+  //    var phoneNumber = document.getelementsByName("ph_number");
+  //    var emailAddress = document.get getelementsByName("email");
+
+
+      var nameArray = document.getElementsByTagName("input");
+
+      if(nameArray[0].value.length == 0){
+        console.log("First Name is Empty. Please Enter First Name");
+      }
+      if(nameArray[1].value.length == 0){
+        console.log("Last Name is Empty. Please Enter Last Name");
+      }
+
+
+      for (var i = 2; i<nameArray.length; i++)
+
+        console.log( nameArray[i].value);
+        var theInput = nameArray[i].value;
+        var thePattern = new RegExp(nameArray[i].pattern);
+        var theTest = thePattern.test(theInput);
+        console.log(theTest);
+
+    });
